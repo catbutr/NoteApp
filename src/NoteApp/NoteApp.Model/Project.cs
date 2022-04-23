cdb3e23c54f77793531cb5cb2b6d75498a04ad31
catbutr <pkarabatov@gmail.com>
2022-04-23 20:39:54 +0700

    Projects class

---
+++ src/NoteApp/NoteApp.Model/Project.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp.Model
+{
+    /// <summary>
+    /// Представление сборника заметок
+    /// </summary>
+    public class Project
+    {
+        /// <summary>
+        /// Сборник заметок
+        /// </summary>
+        private Note[] _projects;
+
+        /// <summary>
+        /// Геттеры и сеттеры для сборника заметок
+        /// </summary>
+        public Note[] Projects
+        {
+            get
+            {
+                return _projects;
+            }
+            set
+            {
+                _projects = value;
+            }
+        }
+
+
+    }
+}
