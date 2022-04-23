002a5de93f02bd3050fc4da730a9424dcb94f90c
catbutr <pkarabatov@gmail.com>
2022-04-23 22:58:37 +0700

    ProjectManager class

---
+++ src/NoteApp/NoteApp.Model/ProjectManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace NoteApp.Model
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProjectManager
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        private string _serialized;
+
+        private const string path = @"..\\My Documents\\NoteApp.notes";
+
+        public ProjectManager(Project myProjects = null)
+        {
+            _serialized = JsonConvert.SerializeObject(myProjects);
+            using (FileStream fstream = new FileStream(path, FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] buffer = Encoding.Default.GetBytes(_serialized);
+                // запись массива байтов в файл
+                fstream.Write(buffer, 0, buffer.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+        }
+
+        public Note Deserialize()
+        {
+            Note newNote = JsonConvert.DeserializeObject<Note>(_serialized);
+            return newNote;
+        }
+    }
+}
